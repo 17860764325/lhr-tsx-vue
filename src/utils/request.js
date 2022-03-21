5e465e6219b023c7ef215becd8e2de1346748b6d
lihaoran <1258774709@qq.com>
2022-03-21 23:23:44 +0800

    添加请求封装request（发送请求前拦截，收到响应后拦截）

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+// 这里面是对请求的操作
+import axios from 'axios'
+
+// create an axios instance
+const service = axios.create({
+  timeout: 5000 // 请求超时时间设置
+})
+
+// 返回体中需要进行的操作
+// service.interceptors.response.use(
+//   response => {
+//     const res = response.data
+//     // if the custom code is not 20000, it is judged as an error.
+//     if (res.status !== 100) {
+//     } else {
+//       return res
+//     }
+//   },
+//   error => {
+//     console.log('err' + error) // for debug
+//     Message({
+//       message: error.message,
+//       type: 'error',
+//       duration: 5 * 1000
+//     })
+//     return Promise.reject(error)
+//   }
+// )
+export default service
